720b6a45cfc20260ef89cfa20c3cb8591a2d2bf1
Ji-Yang Lin <xuunnis123@gmail.com>
2021-06-04 11:22:38 +0800

    add some materials and some configuration

---
+++ frontend/src/products.js
@@ -0,0 +1,110 @@
+const products = [
+  {
+    '_id': '1',
+    'name': 'Beach',
+    'image': '/images/beach.jpg',
+    'description':
+      'Walk Along the beach.',
+    'brand': 'Beach Tour',
+    'category': 'Tourism',
+    'price': 39.9,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'City',
+    'image': '/images/city.jpg',
+    'description':
+      'City',
+    'brand': 'Lions',
+    'category': 'Tourism',
+    'price': 59.99,
+    'countInStock': 7,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Desert',
+    'image': '/images/desert.jpg',
+    'description':
+      'Desert',
+    'brand': 'Happy Camels',
+    'category': 'Tourism',
+    'price': 99.99,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'farm',
+    'image': '/images/farm.jpg',
+    'description':
+      'Farm',
+    'brand': 'Farmer',
+    'category': 'Tourism',
+    'price': 19.99,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Forest',
+    'image': '/images/forest.jpg',
+    'description':
+      'Forest',
+    'brand': 'Animals Paradise',
+    'category': 'Tourism',
+    'price': 69.99,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Leaf-Falled',
+    'image': '/images/leaf.jpg',
+    'description':
+      'Leaf',
+    'brand': 'Taiwan',
+    'category': 'Tourism',
+    'price': 49.99,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+  {
+    '_id': '7',
+    'name': 'Mountain',
+    'image': '/images/mountain.jpg',
+    'description':
+      'Climb me?',
+    'brand': 'Grab',
+    'category': 'Tourism',
+    'price': 19.99,
+    'countInStock': 5,
+    'rating': 3.5,
+    'numReviews': 22,
+  },
+  {
+    '_id': '8',
+    'name': 'Water',
+    'image': '/images/water.jpg',
+    'description':
+      'Swimming or Diving',
+    'brand': 'Water World',
+    'category': 'Tourism',
+    'price': 69.99,
+    'countInStock': 2,
+    'rating': 4.5,
+    'numReviews': 21,
+  },
+  
+]
+
+
+export default products
