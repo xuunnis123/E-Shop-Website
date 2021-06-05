ce2c8dbb3b57eaa75a2027e965b48480d98f323e
Ji-Yang Lin <xuunnis123@gmail.com>
2021-06-05 13:13:05 +0800

    add Router and Route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Navbar, Nav,  Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">Ezra's Web</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>購物車</Nav.Link>
-                        <Nav.Link href="/login"><i className="fas fa-user"></i>登入</Nav.Link>
-                    
-                    </Nav>
-                    
-                </Navbar.Collapse>
+
+                    <LinkContainer to='/'>
+
+                        <Navbar.Brand>Ezra's Web</Navbar.Brand>
+
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i>購物車</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                            <Nav.Link><i className="fas fa-user"></i>登入</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                        
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
