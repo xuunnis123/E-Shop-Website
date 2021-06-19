8b806607244e82ae6233692d428c249fe5e3c286
Ji-Yang Lin <xuunnis123@gmail.com>
2021-06-19 10:58:22 +0800

    add Loader, Message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 
@@ -20,8 +22,8 @@ function HomeScreen() {
     return (
         <div>
             <h1>最新行程</h1>
-            {loading ? <h2>讀取中...</h2>
-            : error ? <h3>{error}</h3>
+            {loading ? <Loader/>
+            : error ? <Message variant='danger'>{error}</Message>
                 : 
                 <Row>
                     {products.map(product => (
