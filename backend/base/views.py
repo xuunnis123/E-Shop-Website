ecdc9b111db4455195b0dcb104570a921f445e9c
Ji-Yang Lin <xuunnis123@gmail.com>
2021-06-30 16:45:20 +0800

    add register user

--- backend/base/views.py
+++ backend/base/views.py
@@ -11,7 +11,8 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -25,25 +26,25 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes=[
-    '/api/products/', 
-    '/api/products/create/',
+@api_view(['POST'])
+def registerUser(request):
 
-    '/api/products/upload/',
-    
-    '/api/products/<id>/reviews/',
-    
-    '/api/products/top/',
-    '/api/products/<id>/',
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
 
-    '/api/products/delete/<id>/',
-    '/api/prodcuts/<update>/<id>/',
+        )
 
-    ]
-    return Response(routes)
-
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'使用者信箱已經存在'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+        
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
@@ -63,7 +64,6 @@ def getProducts(request):
     product = Product.objects.all()
     serializer = ProductSerializer(product, many=True)
     return Response( serializer.data )
-
 
 
 
