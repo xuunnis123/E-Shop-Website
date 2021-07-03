149c814e40bc3060577749411413aa0d27fd5e31
Ji-Yang Lin <xuunnis123@gmail.com>
2021-07-03 15:44:03 +0800

    add front-end side : User Login Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,20 +3,25 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers ,productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-
+import { userLoginReducers} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducer,
     cart:cartReducer,
+    userLogin:userLoginReducers,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')):[]
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')): null
+
 
 const initialState = {
-    cart:{ cartItems:cartItemsFromStorage }
+    cart:{ cartItems:cartItemsFromStorage },
+    userLogin:{ userInfo : userInfoFromStorage }
 }
 
 const middleware = [thunk]
