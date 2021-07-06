8de8be85a6877e8b7b23d7bd46bf58b289a70941
Ji-Yang Lin <xuunnis123@gmail.com>
2021-07-06 11:49:13 +0800

    add update profile endpoint

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -43,7 +43,27 @@ def registerUser(request):
     except:
         message = {'detail':'使用者信箱已經存在'}
         return Response(message,status=status.HTTP_400_BAD_REQUEST)
-        
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many = False)
+
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+    
+    return Response( serializer.data )
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
