316c2bcf862ae0db7293f448266a788bbec89d58
Ji-Yang Lin <xuunnis123@gmail.com>
2021-07-06 16:40:57 +0800

    add ShippingScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/login' component={LoginScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/product/:id' component={ProductScreen}  />
           <Route path='/cart/:id?' component={CartScreen}  />
         </Container>
---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,14 @@
+import React,{ useState, useEffect } from 'react'
+import { Form, Button} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+
+function ShippingScreen() {
+    return (
+        <FormContainer>
+            運送頁面
+        </FormContainer>
+    )
+}
+
+export default ShippingScreen
