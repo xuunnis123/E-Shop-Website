d9ff9662acd958adbd96b6850d276132c6934610
Ji-Yang Lin <xuunnis123@gmail.com>
2021-07-10 17:12:39 +0800

    add Order backend logistic

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -100,3 +100,38 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only = True)
+    ShippingAddress = serializers.SerializerMethodField(read_only = True)
+    user = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orders(self, obj):
+        items = obj.orderItem_set.all()
+        serializer=OrderItemSerializer(items, many = True)
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address=ShippingAddressSerializer(obj.address, many=False)
+        except :
+            address= False
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer=UserSerializer(user, many = False)
+        return serializer.data
